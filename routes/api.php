a20006739f425677efcd464ba985297a060bf57d
ThanhThuy <nttthuy1403@gmail.com>
2025-11-27 10:33:38 +0700

    Initial 1

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,7 @@ use App\Http\Controllers\KhachHangController;
 use App\Http\Controllers\PhieuDatLichController;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/phieu_dat_lich", [PhieuDatLichController::class,"dat_lich_kham"]);
-Route::post("/danh_gia", [PhieuDatLichController::class,"danh_gia"]);
+
 
 // routes/api.php
 Route::get('/test', function () {
